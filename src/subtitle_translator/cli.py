1e498c60ca772f1d1d56eae7e050f862e2b66ba6
枫子 <maple_leaf_7@msn.com>
2024-11-28 20:23:23 +0800

    增加多语言模块，支持了cli中的多语言翻译

--- src/subtitle_translator/cli.py
+++ src/subtitle_translator/cli.py
@@ -11,21 +11,17 @@ localizer = Location()
 # 检测系统语言并设置
 system_language = localizer.detect_system_language()
 localizer.set_language(system_language)
+_ = localizer.get_text
+
 # localizer.set_language("en")
 
 @app.command()
 def main(
     input_file: str,  # 必填参数：输入的字幕文件路径
-    config_file: str = typer.Option("config.ini", help=localizer.get_text("config-patch")),  # 可选参数，带默认值
-    target_language: str = typer.Option("zh", help="目标语言，默认为中文")  # 新增参数，默认为中文
+    config_file: str = typer.Option("config.ini", help=_("config-patch")),  # 可选参数，带默认值
+    target_language: str = typer.Option("zh", help=_("config-default-is-zh"))  # 新增参数，默认为中文
 ):
-    """
-    翻译字幕文件
 
-    :param input_file: 输入的 SRT 文件路径
-    :param config_file: 配置文件路径
-    :param target_language: 目标语言
-    """
     # 从配置文件读取配置项
     api_key, base_url, model, temperature, debug_mode, batch_size, log_enabled, empty_line_placeholder = read_config(config_file)
     enable_logging(log_enabled)
@@ -39,8 +35,8 @@ def main(
         temperature=temperature,
         target_language=target_language  # 传入目标语言
     )
-    typer.echo(f"双语字幕翻译完成，输出文件：{output_bilingual_file}")
-    typer.echo(f"单语字幕翻译完成，输出文件：{output_target_lang_file}")
+    typer.echo(_("output-bilingual-sub").format(file=output_bilingual_file))
+    typer.echo(_("output-single-sub").format(file=output_bilingual_file))
 
 if __name__ == "__main__":
     app()
